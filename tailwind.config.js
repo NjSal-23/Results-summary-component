6bfc4d66764b0531bb0f231873e214d0c9fb8df4
NjSal-23 <Salazar_Luis@Outlook.com>
2023-06-09 11:20:19 -0400

    finalize project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      'purple': '#603efb',
+      'gray': '#d3c3ff',
+      'dark-gray': '#4d4d5b',
+      'white': '#ffffff',
+      'salmon': '#d36e6e',
+      'light-salmon': '#fef6f5',
+      'yellow': '#eab050',
+      'light-yellow': '#fefbf2',
+      'green': '#34a78b',
+      'light-green': '#f3fbfa',
+      'blue': '#3840a7',
+      'light-blue': '#f2f3fd',
+      'shadow-purple': '#4629cf',
+
+    },
+    extend: {
+      // fontSize: {
+      //  'font-size': '0.75rem';, /* 12px */
+      //  'line-height': '1rem'; /* 16px */
+      // },
+    },
   },
   plugins: [],
 }
